76c473c967b485c9884587257bfbd4386a8c5bd7
ahmadliaydin <aydin.ahmadli@merck.com>
2021-04-30 01:32:16 +0400

    update

--- CTMP/model/run_model.py
+++ CTMP/model/run_model.py
@@ -40,7 +40,7 @@ def main():
 
     os.chdir("..")
     utilities.write_setting(ddict, file_name)
-    os.chdir("./iiidirrrr/")
+    os.chdir("./iiiidirrrr/")
 
     wordids, wordcts = utilities.read_data(docs_file)
 
