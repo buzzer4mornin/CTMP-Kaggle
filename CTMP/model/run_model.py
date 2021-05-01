261be56552dc3d248e91e3f484312c17c2f0ee54
ahmadliaydin <aydin.ahmadli@merck.com>
2021-05-02 01:04:10 +0400

    RUNNABLE

--- CTMP/model/run_model.py
+++ CTMP/model/run_model.py
@@ -40,7 +40,7 @@ def main():
 
     os.chdir("..")
     utilities.write_setting(ddict, file_name)
-    os.chdir("./qqdurrr/")
+    os.chdir("./qqqdurrr/")
 
     wordids, wordcts = utilities.read_data(docs_file)
 
