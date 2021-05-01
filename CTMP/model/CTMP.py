f8c18f623f3f617726501024e17e137981536fb1
ahmadliaydin <aydin.ahmadli@merck.com>
2021-05-02 00:21:19 +0400

    RUNNABLE

--- CTMP/model/CTMP.py
+++ CTMP/model/CTMP.py
@@ -60,7 +60,7 @@ class MyCTMP:
 
         # Create small 3D matrices and add them into list
         thousand_block_size = self.user_size // 1000
-        phi = np.empty(shape=(1000, self.num_docs, self.num_topics))
+        phi = np.empty(shape=(1000, self.num_docs, self.num_topics), dtype=np.float32)
         for i in range(1000):
             phi[i, :, :] = block_2D
         for i in range(thousand_block_size):
@@ -68,7 +68,7 @@ class MyCTMP:
 
         # Create last remaining 3D matrix and add it into list
         remaining_block_size = self.user_size % 1000
-        phi = np.empty(shape=(remaining_block_size, self.num_docs, self.num_topics))
+        phi = np.empty(shape=(remaining_block_size, self.num_docs, self.num_topics), dtype=np.float32)
         for i in range(remaining_block_size):
             phi[i, :, :] = block_2D
         phi_matrices.append(phi)
